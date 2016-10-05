2b3d8e4e954f6deb98a098674119587b6b547edc
Bios <bios@Bios.local>
2016-10-05 17:42:25 -0400

    implement 'how' for function hi()

--- R/hi.R
+++ R/hi.R
@@ -6,21 +6,31 @@
 #' @param who charater(1) The name of the person you wish to start a
 #'     conversation with.
 #'
+#' @param how character(1) How to greet the conversation. Either "shout"
+#'     (default) or whisper.
+#'
 #' @return character(1) A line of text to be used when starting conversations.
 #'
 #' @examples
 #' hi("Martin Morgan")
+#' hi("Martin Morgan", "whisper")
 #'
 #' @export
-hi <- function(who) {
+hi <- function(who, how=c("shout", "whisper")) {
     stopifnot(
         is.character(who),
         length(who) == 1,
         !is.na(who)
     )
-    paste("hello", shout(who), "you have", nchar(who), "letters in your name!")
+
+    fun <- switch(match.arg(how), shout=shout, whisper=whisper)
+    paste("hello", fun(who), "you have", nchar(who), "letters in your name!")
 }
 
 shout <- function(who) {
     toupper(who)
+}
+
+whisper <- function(who) {
+    tolower(who)
 }
